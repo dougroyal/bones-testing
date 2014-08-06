fc156ba420848eef9409cfdccc82464eb54292fa
Doug Royal <email@dougroyal.com>
2014-08-06 08:13:47 -0500

    added test_ prefix transformer to fndefs with bdd_blocks

--- bones/conformer.py
+++ bones/conformer.py
@@ -8,20 +8,29 @@ from bones.containers.funcdef import FuncDef
 def suppress_mutations(bag_of_bones):
     new_bones = BagOfBones()
 
-    for i, fn in enumerate(bag_of_bones.funcdefs):
-        sig = _normalize_signature(fn.signature)
+    for i, orig_funcdef in enumerate(bag_of_bones.funcdefs):
         norm_funcdef = FuncDef()
+
+        sig = _remove_heinous_characters(orig_funcdef.signature)
+        sig = _prefix_test_to_tests(orig_funcdef, sig)
         norm_funcdef.signature = sig
+
         new_bones.funcdefs.append(norm_funcdef)
 
     return new_bones
 
 
-def _normalize_signature(sig_toks):
+def _remove_heinous_characters(sig_toks):
     sig_toks.name_tok.type = NAME
     sig_toks.name_tok.value = _correct_func_name(sig_toks.name_tok.value)
     return sig_toks
 
+
+def _prefix_test_to_tests(orig_fn, new_sig):
+    if len(orig_fn.bdd_blocks['then']) > 0:
+        new_sig.name_tok.value = 'test_' + new_sig.name_tok.value
+
+    return new_sig
 
 def _correct_func_name(func_name):
     return re.sub(r'[^0-9a-zA-Z_]+','_', func_name)
--- tests/conformer_tests.py
+++ tests/conformer_tests.py
@@ -43,6 +43,40 @@ def im_a_regular_function():
         self.assertEqual(expected_0, actual.funcdefs[0].signature)
         self.assertEqual(expected_1, actual.funcdefs[1].signature)
 
+    def test_funcdefs_with_bdd_blocks_are_prefixed_with_test_(self):
+        # note: the prefix is 'test_' not 'test'
+        expected_0 = [
+            Token((NAME,'def',(1, 0),(1, 3),'def "I\'m a string funcdef. Wheee!"():\n')),
+            Token((NAME,'test__I_m_a_string_funcdef_Wheee_',(1, 4),(1, 34),'def "I\'m a string funcdef. Wheee!"():\n')),
+            Token((OP,'(',(1, 34),(1, 35),'def "I\'m a string funcdef. Wheee!"():\n')),
+            Token((OP,')',(1, 35),(1, 36),'def "I\'m a string funcdef. Wheee!"():\n')),
+            Token((OP,':',(1, 36),(1, 37),'def "I\'m a string funcdef. Wheee!"():\n')),
+            Token((NEWLINE,'\n',(1, 37),(1, 38),'def "I\'m a string funcdef. Wheee!"():\n')),
+        ]
+        expected_1 = [
+            Token((NAME,'def',(5, 0),(5, 3),'def im_a_regular_function():\n')),
+            Token((NAME,'im_a_regular_function',(5, 4),(5, 25),'def im_a_regular_function():\n')),
+            Token((OP,'(',(5, 25),(5, 26),'def im_a_regular_function():\n')),
+            Token((OP,')',(5, 26),(5, 27),'def im_a_regular_function():\n')),
+            Token((OP,':',(5, 27),(5, 28),'def im_a_regular_function():\n')),
+            Token((NEWLINE,'\n',(5, 28),(5, 29),'def im_a_regular_function():\n')),
+        ]
+
+        data = '''\
+def "I'm a string funcdef. Wheee!"():
+    then:
+        pass
+
+def im_a_regular_function():
+    pass
+'''
+        bag_of_bones = _build_bag_of_bones(data)
+
+        actual = suppress_mutations(bag_of_bones)
+
+        self.assertEqual(expected_0, actual.funcdefs[0].signature)
+        self.assertEqual(expected_1, actual.funcdefs[1].signature)
+
 
 def _build_bag_of_bones(data):
     original_tokens = generate_tokens(StringIO(data))
