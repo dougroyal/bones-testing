d2d53c5ff9e4e52497ec97b1e8b16b3ea99f0e9c
Doug Royal <email@dougroyal.com>
2014-08-13 21:49:19 -0500

    adding indent padding to builder

--- bones/utils/builder.py
+++ bones/utils/builder.py
@@ -1,2 +1,9 @@
+from token import INDENT
+
+
 def build_line(line):
-    return ''.join([tok.value for tok in line])
+    # the INDENT token is only present on the first line of a Block... so, if
+    # we have the line with the INDENT block, there's no need for a padded indent string
+    # otherwise, we need to build the padded string based off the start_col of the first token
+    indent_string = '' if line[0].type == INDENT else ' '*line[0].start_col
+    return indent_string + ''.join([tok.value for tok in line])
--- tests/test_builder.py
+++ tests/test_builder.py
@@ -5,9 +5,8 @@ from bones.utils.builder import build_line
 
 
 class TestBuilder(TestCase):
-
-    def test_line_should_be_built_from_tokens(self):
-        line = [
+    def test_line_w_indent_should_be_built_from_tokens(self):
+        line_w_indent = [
             Token((INDENT, '    ', (3, 0), (3, 4), "    self.assertEqual(foo, bar(bat='baz'))\n")),
             Token((NAME, 'self', (3, 4), (3, 8), "    self.assertEqual(foo, bar(bat='baz'))\n")),
             Token((OP, '.', (3, 8), (3, 9), "    self.assertEqual(foo, bar(bat='baz'))\n")),
@@ -22,9 +21,29 @@ class TestBuilder(TestCase):
             Token((STRING, "'baz'", (3, 34), (3, 39), "    self.assertEqual(foo, bar(bat='baz'))\n")),
             Token((OP, ')', (3, 39), (3, 40), "    self.assertEqual(foo, bar(bat='baz'))\n")),
             Token((OP, ')', (3, 40), (3, 41), "    self.assertEqual(foo, bar(bat='baz'))\n")),
-            Token((NEWLINE,'\n',(3, 41),(3, 42),"    self.assertEqual(foo, bar(bat='baz'))\n"))
-        ]
+            Token((NEWLINE, '\n', (3, 41), (3, 42), "    self.assertEqual(foo, bar(bat='baz'))\n"))]
 
-        actual = build_line(line)
+        actual = build_line(line_w_indent)
+
+        self.assertEqual("    self.assertEqual(foo,bar(bat='baz'))\n", actual)
+
+    def test_line_without_indent_should_be_build_from_tokens(self):
+        line_wo_indent = [
+            Token((NAME, 'self', (3, 4), (3, 8), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((OP, '.', (3, 8), (3, 9), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((NAME, 'assertEqual', (3, 9), (3, 20), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((OP, '(', (3, 20), (3, 21), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((NAME, 'foo', (3, 21), (3, 24), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((OP, ',', (3, 24), (3, 25), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((NAME, 'bar', (3, 26), (3, 29), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((OP, '(', (3, 29), (3, 30), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((NAME, 'bat', (3, 30), (3, 33), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((OP, '=', (3, 33), (3, 34), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((STRING, "'baz'", (3, 34), (3, 39), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((OP, ')', (3, 39), (3, 40), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((OP, ')', (3, 40), (3, 41), "    self.assertEqual(foo, bar(bat='baz'))\n")),
+            Token((NEWLINE, '\n', (3, 41), (3, 42), "    self.assertEqual(foo, bar(bat='baz'))\n"))]
+
+        actual = build_line(line_wo_indent)
 
         self.assertEqual("    self.assertEqual(foo,bar(bat='baz'))\n", actual)
