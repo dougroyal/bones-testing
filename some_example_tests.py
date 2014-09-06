f792c6928077d7262305ac034787ee74220071d6
Doug Royal <email@dougroyal.com>
2014-09-05 20:58:15 -0500

    adding an example to the bones test file

--- some_example_tests.py
+++ some_example_tests.py
@@ -1,11 +1,13 @@
+def look_ma_i_dont_need_the_word_test_at_the_beginning_of_my_function_name():
+    a=b=0
+
+    then:
+        a == b
+
 def "i like string function names because they're easy to read and write"():
-    x=y=0
+    e=f=0
 
     then:
-        x == y
+        e == f
 
-def "more tests"():
-    x=y=0
 
-    then:
-        x == y
