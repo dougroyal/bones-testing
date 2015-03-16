6f0ef7e005cbfb0a5d06b89aefc0840758a0af80
Doug Royal <douglasroyal@gmail.com>
2015-03-16 12:20:01 -0500

    refactor block creation

--- bones/token_parser.py
+++ bones/token_parser.py
@@ -8,20 +8,11 @@ def parse(tokens):
 
     for index, tok in enumerate(tokens):
 
-        if _is_start_of_class_block(tok):
-            curr_container = Block(block_type=CLASS, parent=module)
-            module.children.append(curr_container)
-
-        elif _is_start_of_function_block(tok):
-            func_block = Block(block_type=FUNCTION, parent=curr_container)
-            curr_container.children.append(func_block)
-            curr_container = func_block
-
-        elif _is_start_of_bdd_block(tok):
-            bdd_block = Block(block_type=BDD_BLOCK, parent=curr_container)
-            curr_container.children.append(bdd_block)
-            curr_container = bdd_block
-
+        new_block_type = _is_start_of_block(tok)
+        if new_block_type:
+            block = Block(block_type=new_block_type, parent=curr_container)
+            curr_container.children.append(block)
+            curr_container = block
 
         curr_container.tokens.append(tok)
 
@@ -31,13 +22,15 @@ def parse(tokens):
     return module
 
 
-def _is_start_of_class_block(tok):
-    return tok.type == NAME and tok.string == 'class'
+def _is_start_of_block(tok):
+    if tok.type != NAME:
+        return False
 
+    if tok.string == 'class':
+        return CLASS
 
-def _is_start_of_function_block(tok):
-    return tok.type == NAME and tok.string == 'def'
+    if tok.string == 'def':
+        return FUNCTION
 
-
-def _is_start_of_bdd_block(tok):
-    return tok.type == NAME and tok.string in BDD_BLOCK_TYPES
+    if tok.string in BDD_BLOCK_TYPES:
+        return BDD_BLOCK
